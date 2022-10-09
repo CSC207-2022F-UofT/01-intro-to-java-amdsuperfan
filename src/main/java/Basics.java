df6e5860273db3ec636efd57f84dd8157587d17a
amdsuperfan <yujun.wu@mail.utoronto.ca>
2022-10-09 18:46:58 -0400

    new submission

--- src/main/java/Basics.java
+++ src/main/java/Basics.java
@@ -39,7 +39,7 @@ public class Basics {
          *    (Relevant reading: 1.1.3. Printing things)
          */
         System.out.println(7 + 5);
-
+        System.out.println("Hello World!");
         /* TODO (Task 1): Write a line of code below that prints the string
          *                Hello World!
          */
@@ -62,7 +62,7 @@ public class Basics {
          * TODO (Task 2): Create a variable named my_variable and assign it the
          *                value 100.
          */
-
+        int my_variable = 100;
 
 
         /* Do not remove the line below: if you did task 2 correctly, then
@@ -99,7 +99,9 @@ public class Basics {
          * Current count: 1
          * Current count: 0
          */
-
+        for (int i = 10; i >= 0; i--){
+            System.out.println("Current count:" + i);
+                 }
 
     }
 
@@ -141,7 +143,12 @@ public class Basics {
          *                you may use them if you wish.
          */
         StringBuilder ret = new StringBuilder();
-
+        ret.append(to_split.charAt(0));
+        for (int i = 0; i <= to_split.length(); i++){
+            if(to_split.charAt(i).equals(" ")){
+                ret.append(to_split.charAt(i+1));
+            };
+        }
         // Fill in the rest of the body here
 
         return ret.toString();
@@ -163,7 +170,11 @@ public class Basics {
      */
     public static int oddSum(int[] arr) {
         int current_sum = 0;
-
+        if(arr.length > 1){
+            for(int i = 0; i <= arr.length; i++){
+                current_sum = current_sum + arr[i];
+            }
+        }
         /* TODO (Task 5): Complete this method body using a for-loop.
          *                You can find the length of an array by using the
          *                .length attribute (e.g. arr.length)
